0b820fef3c778e5925fa26d7cb39fde0175a1264
Nick Woodhams <nicholas.woodhams@gmail.com>
2014-08-04 13:11:06 -0400

    updates to the store

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 var overgrowthApp = angular.module('overgrowthApp', []);
 
-overgrowthApp.controller('priceController', function($scope) {
+overgrowthApp.controller('overgrowthController', function($scope) {
 
     $scope.store = {
         'publications': {
@@ -69,6 +69,9 @@ overgrowthApp.controller('priceController', function($scope) {
         $scope.current_variation = $scope.current_product.products[0];
         $scope.current_variation.selected_size = $scope.current_variation.sizes[0];
         $scope.current_variation.selected_quantity = 1;
+        if (_.has($scope.current_variation, 'numbers')) {
+            $scope.current_variation.selected_number = 1;
+        }
         return false;
     };
 
@@ -78,10 +81,16 @@ overgrowthApp.controller('priceController', function($scope) {
             $scope.current_variation = $scope.current_product.products[idx];
             $scope.current_variation.selected_size = $scope.current_variation.sizes[0];
             $scope.current_variation.selected_quantity = 1;
+            if (_.has($scope.current_variation, 'numbers')) {
+                $scope.current_variation.selected_number = 1;
+            }
         } else {
             $scope.current_variation = _.first($scope.current_product.products);
             $scope.current_variation.selected_size = $scope.current_variation.sizes[0];
             $scope.current_variation.selected_quantity = 1;
+            if (_.has($scope.current_variation, 'numbers')) {
+                $scope.current_variation.selected_number = 1;
+            }
         }
         return false;
     };
@@ -92,10 +101,16 @@ overgrowthApp.controller('priceController', function($scope) {
             $scope.current_variation = $scope.current_product.products[idx];
             $scope.current_variation.selected_size = $scope.current_variation.sizes[0];
             $scope.current_variation.selected_quantity = 1;
+            if (_.has($scope.current_variation, 'numbers')) {
+                $scope.current_variation.selected_number = 1;
+            }
         } else {
             $scope.current_variation = _.last($scope.current_product.products);
             $scope.current_variation.selected_size = $scope.current_variation.sizes[0];
             $scope.current_variation.selected_quantity = 1;
+            if (_.has($scope.current_variation, 'numbers')) {
+                $scope.current_variation.selected_number = 1;
+            }
         }
         return false;
     };
@@ -113,41 +128,40 @@ overgrowthApp.controller('priceController', function($scope) {
     };
 
     $scope.nextSize = function() {
-        var idx = _.indexOf($scope.current_variation.sizes, $scope.current_variation) + 1;
+        var idx = _.indexOf($scope.current_variation.sizes, $scope.current_variation.selected_size) + 1;
         if (_.has($scope.current_variation.sizes, idx)) {
             $scope.current_variation.selected_size = $scope.current_variation.sizes[idx];
         } else {
             $scope.current_variation.selected_size = _.first($scope.current_variation.sizes);
-            console.log(_.first($scope.current_variation.sizes));
         }
         return false;
     };
 
     $scope.previousSize = function() {
-        var idx = _.indexOf($scope.current_variation.sizes, $scope.current_variation) - 1;
+        var idx = _.indexOf($scope.current_variation.sizes, $scope.current_variation.selected_size) - 1;
         if (_.has($scope.current_variation.sizes, idx)) {
             $scope.current_variation.selected_size = $scope.current_variation.sizes[idx];
         } else {
             $scope.current_variation.selected_size = _.last($scope.current_variation.sizes);
-
         }
         return false;
     };
 
-    $scope.current_product = $scope.store.publications;
-    $scope.current_variation = $scope.current_product.products[0];
-    $scope.current_variation.selected_size = $scope.current_variation.sizes[0];
-    $scope.current_variation.selected_quantity = 1;
 
+    $scope.nextNumber = function() {
+        if ($scope.current_variation.selected_number < _.max($scope.current_variation.numbers)) {
+            $scope.current_variation.selected_number = $scope.current_variation.selected_number + 1;
+        }
+        return false;
+    };
 
-
-});
-
-
-
-
-
-overgrowthApp.controller('carouselContentController', function($scope) {
+    $scope.previousNumber = function() {
+        _.min($scope.current_variation.numbers);
+        if ($scope.current_variation.selected_number > _.min($scope.current_variation.numbers)) {
+            $scope.current_variation.selected_number = $scope.current_variation.selected_number - 1;
+        }
+        return false;
+    };
 
     $scope.priceRange = function(product_type) {
         var r = [];
@@ -161,14 +175,73 @@ overgrowthApp.controller('carouselContentController', function($scope) {
         return {'max': max, 'min': min};
     };
 
+    $scope.stripeCheckout = function() {
+        // Open Checkout with further options
+        var handler = StripeCheckout.configure({
+            key: 'pk_test_4O1woQIrDngFxcp9btGjkFiL',
+            image: 'static/img/girl-face-square.jpg',
+            token: function(token) {
+            // Use the token to create the charge with a server-side script.
+            // You can access the token ID with `token.id`
+            }
+        });
+        var total = $scope.current_variation.selected_size.price * $scope.current_variation.selected_quantity;
+        var description = $scope.current_variation.selected_quantity + " " + $scope.current_variation.name + " - " + $scope.current_variation.selected_size.sized;
+        if ($scope.current_variation.selected_number) {
+            description = description + " No. " + $scope.current_variation.selected_number;
+        }
+        description = description + " ($" + total + ")";
+        handler.open({
+          name: $scope.current_product.section,
+          description: description,
+          amount: total * 100,
+          address: true
+        });
+    };
 
-
-
-
-
-
+    $scope.current_product = $scope.store.publications;
+    $scope.current_variation = $scope.current_product.products[0];
+    $scope.current_variation.selected_size = $scope.current_variation.sizes[0];
+    $scope.current_variation.selected_quantity = 1;
+    if (_.has($scope.current_variation, 'numbers')) {
+        $scope.current_variation.selected_number = 1;
+    }
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
