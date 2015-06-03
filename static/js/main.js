749a81fcd88d5dc3c686b855caaf353c137d5571
Karl Golts <kgolts74@gmail.com>
2015-06-02 17:24:13 -0700

    backups

--- static/js/main.js
+++ static/js/main.js
@@ -1,234 +1,373 @@
-/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
-(function(n) {
-    n.viewportSize = {}, n.viewportSize.getHeight = function() {
-        return t("Height")
-    }, n.viewportSize.getWidth = function() {
-        return t("Width")
-    };
-    var t = function(t) {
-        var f, o = t.toLowerCase(),
-            e = n.document,
-            i = e.documentElement,
-            r, u;
-        return n["inner" + t] === undefined ? f = i["client" + t] : n["inner" + t] != i["client" + t] ? (r = e.createElement("body"), r.id = "vpw-test-b", r.style.cssText = "overflow:scroll", u = e.createElement("div"), u.id = "vpw-test-d", u.style.cssText = "position:absolute;top:-1000px", u.innerHTML = "<style>@media(" + o + ":" + i["client" + t] + "px){body#vpw-test-b div#vpw-test-d{" + o + ":7px!important}}<\/style>", r.appendChild(u), i.insertBefore(r, e.head), f = u["offset" + t] == 7 ? i["client" + t] : n["inner" + t], i.removeChild(r)) : f = n["inner" + t], f
-    }
-})(this);
-
-/**
- * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
- * Want to see more similar demos and tutorials?
- * Help by spreading the word about Ihatetomatoes blog.
- * Facebook - https://www.facebook.com/ihatetomatoesblog
- * Twitter - https://twitter.com/ihatetomatoes
- * Google+ - https://plus.google.com/u/0/109859280204979591787/about
- * Article URL: http://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website-part-2/
- */
-
-(function($) {
-
-    // Setup variables
-    $window = $(window);
-    $slide = $('.homeSlide');
-    $slideTall = $('.homeSlideTall');
-    $slideTall2 = $('.homeSlideTall2');
-    $body = $('body');
-    htmlbody = $('html,body');
-    var duration = 500;
-
-    //FadeIn all sections
-    $body.imagesLoaded(function() {
-        setTimeout(function() {
-
-            // Resize sections
-            adjustWindow();
-
-            // Init navigation
-            initHomepageNav();
-
-            // Fade in sections
-            $body.removeClass('loading').addClass('loaded');
-
-        }, 800);
-    });
-
-    function adjustWindow() {
-
-        // Init Skrollr
-        var s = skrollr.init({
-            forceHeight: false,
-            render: function(data) {
-
-                //Debugging - Log the current scroll position.
-                //console.log(data.curTop);
-            }
+  $(function() {
+        
+        //side dot menu functions
+       
+        $('body').scrollspy({
+            target: '#dotted-nav',
+            offset: 300
         });
 
-        // Get window size
-        winH = $window.height();
-
-        // Keep minimum height 550
-        if (winH <= 550) {
-            winH = 550;
-        }
-
-        // Resize our slides
-        $slide.height(winH);
-        $slideTall.height(winH * 2);
-        $slideTall2.height(winH * 3);
-
-        // Refresh Skrollr after resizing our sections
-        s.refresh($('.homeSlide'));
-
-    }
-
-    function initHomepageNav() {
-
-        var homeSlides = $('.homeSlide');
-        var $slideContent = $('.hsContainer');
-        var slidesCount = $(homeSlides).length;
-        var activeSlide = 1;
-
-        // Build HTML for Nav
-        $('<div/>', {
-            'id': 'slideNav'
-        }).append($('<ul><li class="slideNavPrev"><a class="disabled" href="#" title="Go to previous slide"><span class="ico ico-up">↑</span></a></li><li><span id="activeSlide">' + activeSlide + '</span>/<span id="maxSlides">' + slidesCount + '</span></li><li class="slideNavNext"><a href="#" title="Go to next slide"><span class="ico ico-down">↓</span></a></li></ul>')).appendTo('body').delay(1200).fadeIn(duration);
-
-
-        // Navigation highligting
-        var $activeSlide = $('#activeSlide');
-        var $maxSlides = $('#maxSlides');
-        var $numberOfSlides = parseInt($maxSlides.text());
-        var slideNavPrev = $('');
-        var $slideNavNext = $('.slideNavNext');
-        var $slideNavPrev = $('.slideNavPrev');
-        var $slideNavNextA = $('.slideNavNext a');
-        var $slideNavPrevA = $('.slideNavPrev a');
-
-        // Highlight the section currently scrolling DOWN
-        homeSlides.waypoint(function(direction) {
-            if (direction === 'down') {
-                var index = $(this).index();
-                var index = index + 1;
-                $activeSlide.text(index);
-                showHideNavItems();
-            }
-        }, {
-            offset: '50%'
-        });
-
-        // Highlight the section currently scrolling UP
-        homeSlides.waypoint(function(direction) {
-            if (direction === 'up') {
-                var index = $(this).index();
-                var index = index + 1;
-                $activeSlide.text(index);
-                showHideNavItems();
-            }
-        }, {
-            offset: function() {
-                // This is the calculation that would give you
-                // "bottom of element hits middle of window"
-                return $.waypoints('viewportHeight') / 2 - $(this).outerHeight();
-            }
-        });
-
-        //Fade out unnecesary nav items
-
-        function showHideNavItems() {
-            var $activeSlideNumber = parseInt($activeSlide.text());
-
-            if ($activeSlideNumber == 1) {
-
-                $slideNavNextA.removeAttr('class');
-                $slideNavPrev.animate({
-                    opacity: 0.25
-                }).find('a').addClass('disabled');
-
-            } else if ($activeSlideNumber == $numberOfSlides) {
-
-                $slideNavPrevA.removeAttr('class');
-                $slideNavNext.animate({
-                    opacity: 0.25
-                }).find('a').addClass('disabled');
-
+        //animate menu scrolling
+         $('#dotted-nav a,.bottom-right a, #main-nav a').on('click',function (e) {
+          e.preventDefault()
+           var $target = $(this)[0].hash;
+             console.log($target);
+            //   thisAnchor = thisHREF.hash.substring(1);
+           $('html,body').animate({scrollTop:$($target).offset().top}, 700, 'easeOutCubic',function(){
+             window.location.hash = $target;
+           });
+        })
+         
+        function adjustElements() {
+            var active = $('#dotted-nav').find(".active > a").attr('href');
+            if (active == "#about" || active == "#artist-statement" || active == "#store") {
+                $('#dotted-nav').removeClass('light').addClass('dark');
+                $('#overgrowth-image').removeClass('light').addClass('dark');
+                $('#toggle-mobile-menu').removeClass('light').addClass('dark');
             } else {
-
-                $slideNavNext.add($slideNavPrev).animate({
-                    opacity: 1
-                });
-                $slideNavNextA.add($slideNavPrevA).removeAttr('class');
-
+                $('#dotted-nav').removeClass('dark').addClass('light');
+                $('#overgrowth-image').removeClass('dark').addClass('light');
+                $('#toggle-mobile-menu').removeClass('dark').addClass('light');
             }
-        }
-
-        //Next slide
-        $slideNavNext.click(function(e) {
-            e.preventDefault();
-            var index = parseInt($activeSlide.text());
-            index++;
-            if (index <= $numberOfSlides) {
-
-                scrollToSlide(index);
-
-            }
-        });
-
-        //Prev slide
-        $slideNavPrev.click(function(e) {
-            e.preventDefault();
-            var index = parseInt($activeSlide.text());
-            index--;
-            if (index > 0) {
-
-                scrollToSlide(index);
-
-            }
-        });
-
-
-        function scrollToSlide(slideId) {
-
-            // Custom slide content offset
-            var customSlideOffset = $("#slide-" + slideId).attr('data-content-offset');
-
-
-            // Scroll to the top of a container if it doesn't have custom offset defined
-            if (typeof customSlideOffset === 'undefined') {
-
-                htmlbody.animate({
-                    scrollTop: ($("#slide-" + slideId).offset().top) + 'px'
-                }, 'slow');
-
+            if (active == "#store" || active == "#kickstarter") {
+                $('#overgrowth-image').css('opacity', 0);
             } else {
-
-                // Convert percentage 'eg. 25p' into pixels
-                if (customSlideOffset.indexOf('p') != -1) {
-
-                    var customSlideOffset = parseInt(customSlideOffset.split('p')[0]);
-                    var slideHeight = $slide.height();
-
-                    customSlideOffset = Math.ceil((slideHeight / 100) * customSlideOffset);
-
-                    //console.log(slideHeight +' '+ customSlideOffset);
-
-                    htmlbody.animate({
-                        scrollTop: ($("#slide-" + slideId).offset().top + customSlideOffset) + 'px'
-                    }, 'slow');
-
-                } else {
-
-                    var customSlideOffset = parseInt(customSlideOffset);
-
-                    htmlbody.animate({
-                        scrollTop: ($("#slide-" + slideId).offset().top + customSlideOffset) + 'px'
-                    }, 'slow');
-
-                }
-
+                $('#overgrowth-image').css('opacity', 1);
             }
         }
 
+        $('#dotted-nav').on('activate.bs.scrollspy', function(event) {
+            adjustElements();
+        });
 
-    }
+        $('.swipebox').swipebox();
 
-})(jQuery);
+        $('#main-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            removalDelay: 0,
+            mainClass: 'mfp-fade',
+            showCloseBtn: false,
+            gallery: {
+                enabled: true,
+                arrowMarkup: '<a class="gallery-arrow gallery-arrow-%dir% hug"></a>'
+            },
+            preloader: true,
+            tLoading: '<div class="loading"></div>',
+            callbacks: {
+                change: function() {
+                    if (this.isOpen) {
+                        this.wrap.addClass('mfp-open');
+                    }
+                },
+                open: function() {
+                    $('#open-gallery-button').css('opacity', '0');
+                },
+                close: function() {
+                    $('#open-gallery-button').css('opacity', '1');
+                },
+            },
+        });
+
+
+        $('.gallery').each(function() { // the containers for all your galleries
+            $(this).magnificPopup({
+                delegate: 'a', // the selector for gallery item
+                type: 'image',
+                mainClass: 'mfp-fade',
+                preloader: true,
+                tLoading: '<div class="loading"></div>',
+                gallery: {
+                  enabled:true,
+                  arrowMarkup: '<a class="gallery-arrow gallery-arrow-%dir% hug"></a>'
+                },
+                callbacks: {},
+            });
+        });
+
+
+        $('#main-nav, #dotted-nav, #welcome, #overgrowth-fixed').css('opacity', '1.00');
+
+
+        $('.purchase-overlay').click(function() {
+            $('.checkout-overlay').fadeIn();
+            return false;
+        });
+
+        $('#continue-shopping, .checkout-overlay').click(function() {
+            $('.checkout-overlay').fadeOut();
+            return false;
+        });
+
+        $('#toggle-mobile-menu').click(function() {
+            // $('#mobile-nav').show();
+            // console.log($(this).attr('data-open'));
+            if ($(this).attr('data-open') === "false") {
+               // console.log('slidedown');
+                $('#mobile-nav').slideDown();
+                $(this).attr('data-open', "true");
+            } else {
+              //  console.log('slideup');
+                $('#mobile-nav').slideUp();
+                $(this).attr('data-open', "false");
+            }
+            return false;
+        });
+
+        $('#mobile-nav a').click(function() {
+            $('#mobile-nav').slideUp();
+            $('#toggle-mobile-menu').attr('data-open', "false");
+        });
+
+       
+
+
+        });
+        
+        
+   //skrollr
+        
+        ( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('section');
+	$body = $('body');
+	
+  
+	function adjustWindow(){
+
+	    // Get window size
+	   winH = $window.height();
+	    winW = $window.width();
+        
+        if (winH < 775) {
+                $('#store-items').addClass('short-screen');
+            }
+
+	    // Keep minimum height 550
+	    if(winH <= 500) {
+	        winH = 500;
+	    }
+
+	    // Init Skrollr for 768 and up
+	    if( winW >= 768) {
+          
+	        // Init Skrollr
+	         var s = skrollr.init({
+            
+            smoothScrolling: false
+        });
+
+	        // Resize our slides
+	        $slide.height(winH);
+
+	        s.refresh($('section'));
+          
+            setTimeout(scrolltoanchorauto, 1000)
+	    } else {
+
+	        // Init Skrollr
+	         var s = skrollr.init({
+           
+        });
+	        s.destroy();
+	    }
+	
+		// Check for touch
+	   	if(Modernizr.touch) {
+
+			// Init Skrollr
+			var s = skrollr.init();
+			s.destroy();
+	   	}
+
+	}
+	
+	function initAdjustWindow() {
+	    return {
+	        match : function() {
+	            adjustWindow();
+	        },
+	        unmatch : function() {
+	            adjustWindow();
+	        }
+	    };
+	}
+    
+   
+    //turn off skrollr for mobile
+	enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false)     
+    
+    
+   
+    	if(! Modernizr.touch) { 
+            //desktop
+            var doit;
+            function resizedw(){
+              
+               thisAnchor = window.location.hash.substring(1); 
+                var str = "!/";
+                newAnchor = thisAnchor.replace(str, "");
+                window.location.hash = newAnchor;
+              
+                adjustWindow();
+                scrollToAnchor(newAnchor);
+            }
+            $( window ).resize(function()  {
+                clearTimeout(doit);
+                doit = setTimeout(function() {
+                    resizedw();
+                }, 100);
+            });
+            
+        
+        }else{
+            //mobile
+            
+            $(function (){
+                   $(".bg-div-landing").fadeOut(2000, function() {
+                    $("#landing").css("z-index","1");
+                    scrolltoanchorauto();
+                   });
+                $(".hide-mobile").hide();
+                  $(".show-mobile").show();
+                
+              })
+            
+            
+            
+            
+            
+             $( window ).resize(function() {
+                  winH = $window.height() + 60;
+                 $slide = $('section');
+                  $slide.height(winH);
+               //  console.log(winH);
+                  thisAnchor = window.location.hash.substring(1); 
+                var str = "!/";
+                newAnchor = thisAnchor.replace(str, "");
+                window.location.hash = newAnchor;
+              
+               
+                scrollToAnchor(newAnchor);
+                 
+                
+             })
+        }
+            
+            
+        
+		
+} )( jQuery );
+        
+        //end skrollr
+        
+        
+      
+        
+  //vid script
+        
+        if(! Modernizr.touch) { 
+        
+        videojs("main_video").ready(function(){
+            var vidPlayer = this;
+            $('#play-video').on('click', function(e){
+               vidPlayer.play();
+
+            vidPlayer.on('pause', function() {
+                setTimeout(function(){
+                    if( vidPlayer.paused() )  {
+                        $('.pause-overlay').addClass('is-visible');
+                    }
+                }, 200);
+            });
+
+		vidPlayer.on('play', function(){
+			$('.pause-overlay').removeClass('is-visible');
+            	
+		});
+
+		$('#video-resume').on('click', function(e){
+			vidPlayer.play();
+		});
+            
+        $('#video-close').on('click', function(e){
+				$('.pause-overlay').removeClass('is-visible');
+             $('#myModal').fadeOut(function(){ $('#myModal').modal('hide'); }); 
+           
+            
+            })
+
+		
+               
+           })
+
+        }) 
+        }; 
+        
+        
+        
+       
+   //end vid script
+    
+        
+  
+    //scroll to anchor functions
+        
+      function scrolltoanchorauto(){
+          thisAnchor = window.location.hash.substring(1);
+            console.log(thisAnchor + ' -auto');
+            if(thisAnchor != ""){
+                var str = "!/";
+                newAnchor = thisAnchor.replace(str, "");
+                window.location.hash = newAnchor;
+              
+              //  adjustWindow();
+                scrollToAnchor(newAnchor);
+            }
+        }
+        
+    function scrollToAnchor(aid){
+        var aTag = $("section[id='"+ aid +"']");
+       // console.log(aTag);
+        try {
+        $('html,body').animate({scrollTop: aTag.offset().top},0);
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    }    
+    
+    
+ 
+        //cart stuff
+        Snipcart.execute('bind', 'cart.ready', function (data) {
+           
+            if ($("span.snipcart-total-items").text() > 0) {
+                $(".snipcart-summary").addClass("show");
+            }
+         });
+        
+        Snipcart.execute('bind', 'item.added', function (item) {
+             if ($("span.snipcart-total-items").text() > 0) {
+                $(".snipcart-summary").addClass("show");
+            }
+        });
+        
+        Snipcart.execute('bind', 'item.removed', function (item) {
+           if ($("span.snipcart-total-items").text() == 0) {
+                $(".snipcart-summary").removeClass("show");
+            }
+        });
+        
+        Snipcart.execute('bind', 'cart.closed', function() {
+            
+          
+            
+         if ($("span.snipcart-total-items").text() == 0) {
+                $(".snipcart-summary").removeClass("show");
+  
+            }
+        });
